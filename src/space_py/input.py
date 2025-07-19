f85525c40c5efa7e7af9ef11d4bfe95b602c746b
Schrom <schromec@nih.gov>
2025-07-19 19:35:03 -0400

    Sketched plans for classes

--- src/space_py/input.py
+++ src/space_py/input.py
@@ -1,2 +1,11 @@
 import tifffile as tf
 import numpy as np
+
+# Start with classes:
+# Image parent class
+#   ObjectImage
+#   ScalarImage
+# Table parent class
+#   ObjectTable
+#   ProfileTable
+#   LinkTable
